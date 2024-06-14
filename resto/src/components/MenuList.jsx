1cd1c1c2da2b40f7a029a84135dc2d25afc282df
Rosa Benchabane <benchabane.rosa@outlook.com>
2024-06-14 14:03:38 +0200

    install npm & exo

---
+++ resto/src/components/MenuList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MenuItem from './MenuItem';
+
+function MenuList({ foodItems }) {
+  console.log(foodItems);
+  return (
+    <>
+      {/*render a MenuItem component to each element of the props array*/}
+    </>
+  );
+}
+
+export default MenuList;
